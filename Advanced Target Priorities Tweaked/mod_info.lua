ea2d5b3c97a2f401ae0b18bfb2f46e2da1ea4228
MatteoR3d4 <matreda@gmail.com>
2023-12-15 21:48:44 +0100

    Aggiunta Advanced Target Priorities Tweaked

---
+++ Advanced Target Priorities Tweaked/mod_info.lua
@@ -0,0 +1,14 @@
+name = "Advanced Target Priorities Tweaked"
+version = 5
+copyright = "WTFPL"
+description = "Adds more presets and allows to make your own (see forum topic for instructions). Modified from Strogo's v1.1 version. Fix for custom snipe mode priorities and default preset improvements by Nomander."
+author = "Strogo, Nomander"
+url = "https://forums.faforever.com/viewtopic.php?f=41&t=17047"
+icon = "/mods/Advanced Target Priorities Tweaked/icon.png"
+uid = "afc472ed-ab3b-4c0a-a05d-e0ce966d08ee"
+exclusive = false
+ui_only = true
+conflicts = {"6d4ef970-3ffa-4345-afea-78b647afdb6f",	--ATP Tweaked Version 4
+				"081948f9-12fc-40cc-9045-022d98bdf61b", --ATP Tweaked version 2/3
+				"5a5b58f9-5521-494e-8e55-3b0ce3dd0276",	--ATP Tweaked version 1
+				"20dec73e-d4b2-11e5-ab30-625662811111",}--ATP 1.1
