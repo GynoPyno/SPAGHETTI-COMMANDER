3e9563fc50df1b5bf74439887790cb33cac343c2
MatteoR3d4 <matreda@gmail.com>
2024-09-12 22:04:23 +0200

    Aggiunta OOF DEATH SOUND

---
+++ Oof/mod_info.lua
@@ -0,0 +1,8 @@
+name = "OOF ACU Death Sound"
+uid = "BBEFBDF1-3C21-4CD6-89EE-86EA42218C0C"
+version = 2
+description = "Plays OOF during the ACU explosion"
+author = "JJ and quaatal"
+icon = "/mods/Oof/mod_icon.png"
+exclusive = false
+ui_only = false
