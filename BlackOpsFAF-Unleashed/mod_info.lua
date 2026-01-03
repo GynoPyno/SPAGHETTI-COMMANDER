862c00a15c3b2d3d49ca6d0c15b3206c0e90cc70
MatteoR3d4 <matreda@gmail.com>
2026-01-04 00:07:22 +0100

    dio bello le blackops

--- BlackOpsFAF-Unleashed/mod_info.lua
+++ BlackOpsFAF-Unleashed/mod_info.lua
@@ -1,5 +1,5 @@
-name = "MAXI BlackOps FAF: Unleashed MAXI"
-uid = "9e8ea941-c306-aaaf-b367-u00000026000-MAXI"
+name = "BlackOps FAF: Unleashed"
+uid = "9e8ea941-c306-aaaf-b367-u00000026000"
 version = 26
 copyright = "2009 Exavier Macbeth, lt_hawkeye, orangeknight"
 description = "The ultimate unit pack, re-engineered for FAF 3680 or later"
